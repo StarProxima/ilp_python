c307fa2f1ab1e9393cc4504985e5592659098936
StarProxima <maks.prozorov1@yandex.ru>
2022-10-01 11:27:39 +0300

    Lab 12 - Task 3 completed

---
+++ lab_12_3.py
@@ -0,0 +1,48 @@
+# Задание 13 Что за химический элемент. Напишите программу, которая
+# будет считывать файл, содержащий информацию о химических элементах, и
+# сохранять ее в более подходящей для этого структуре данных. После этого
+# пользователь должен ввести значение. Если введенное значение окажется
+# целочисленным, программа должна вывести на экран обозначение и название
+# химического элемента с введенным количеством протонов. При вводе
+# пользователем строки необходимо отобразить количество протонов элемента
+# с введенным пользователем обозначением или названием. Если введенное
+# пользователем значение не соответствует ни одному из элементов в файле,
+# необходимо вывести соответствующее сообщение об ошибке. Позвольте
+# пользователю вводить значения до тех пор, пока он не оставит ввод пустым.
+
+import sys
+
+if len(sys.argv) < 2:
+    print('Usage: lab_12_3.py filename')
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+elements = {}
+
+with open(filename, 'r') as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            name, symbol, number = line.split()
+            elements[name] = (symbol, number)
+            elements[symbol] = (name, number)
+            elements[number] = (symbol, name)
+
+while True:
+    value = input('Enter a number or symbol or name: ')
+    if not value:
+        break
+    try:
+        value = int(value)
+        try:
+            (symbol, name) = elements[str(value)]
+            print(f'{symbol} {name} {value}')
+        except KeyError:
+            print(f'No element with {value} protons')
+    except ValueError:
+        try:
+            (name, number) = elements[value]
+            print(f'{number} {value}')
+        except KeyError:
+            print(f'No element with symbol or name {value}')

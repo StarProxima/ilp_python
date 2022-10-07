f8c532da110fa08e877de1e7c0472daabe2c56a6
StarProxima <maks.prozorov1@yandex.ru>
2022-10-07 13:44:17 +0300

    Lab 13 - Task 2 completed

--- lab_13.py
+++ lab_13.py
@@ -10,3 +10,28 @@ def task1():
 
     cat = Cat("Вася", "amber", 4)
     cat.meow()
+
+
+def task2():
+    class Animal():
+        def __init__(self, name):
+            self.name = name
+            print(f"Родилось животное {name}")
+
+        def eat(self):
+            print("Намнём")
+
+        def setName(self, name):
+            self.name = name
+
+        def getName(self):
+            return self.name
+
+        def MakeNoise(self):
+            print(f"{self.name} говорит: Гррр")
+
+    animal = Animal("Гарфилд")
+    animal.getName()
+    animal.setName("Барсик")
+    animal.eat()
+    animal.MakeNoise()
